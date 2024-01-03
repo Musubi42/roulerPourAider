c137f26fee11be3004ce82527debd7f4f981139c
Musubi42 <thibaut.raphael@protonmail.com>
2024-01-03 15:34:11 +0100

    Add(countdown padge): the user will automatically redirected to countdown page on maintenance mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,17 +18,8 @@ export default {
         "fond-tableau": "url('/assets/images/BG-tableau.png')",
       },
       colors: {
-        primary: "#0000ff",
-        "lilas-pop": "#974dff",
-        "rose-neon": "#ff0066",
-        "bleu-ciel": "#99deff",
-        "jaune-citron": "#ffff00",
-        // J'ai essayé de rendre les couleurs dynamiquement accessible
-        // Si à la compilation elles ne sont pas de ls css du html, qu'on puisse quand même sans servir dynamiquement via JS
-        // "lilas-pop": "var(--lilas-pop)",
-        // "rose-neon": "var(--rose-neon)",
-        // "bleu-ciel": "var(--bleu-ciel)",
-        // "jaune-citron": "var(--jaune-citron)",
+        primary: "#72BC7A",
+        secondary: "#2A5FA4"
       },
       fontFamily: {
         montserrat: ["Montserrat", "Arial", "sans-serif"],
