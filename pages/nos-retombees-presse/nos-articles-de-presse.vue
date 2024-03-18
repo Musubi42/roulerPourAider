e0ceaf6ba07b9daa1e44172b2dd79e3fc2e526a9
Musubi42 <raphaelthi59@gmail.com>
2024-03-18 20:27:51 +0100

    WIP

--- pages/nos-retombees-presse/nos-articles-de-presse.vue
+++ pages/nos-retombees-presse/nos-articles-de-presse.vue
@@ -1,43 +1,120 @@
 <template>
   <div>
-    <h1>Nos articles de presse</h1>
-    <ul>
-      <li v-for="article in articles" >{{ article.attributes.title }}</li>
-    </ul>
+    <section class="flex flex-wrap bg-gray-200 pt-10">
+      <div
+        v-for="(article, index) in articles"
+        :key="index"
+        class="px-4 mb-8 w-4/12 flex flex-col"
+      >
+        <div class="bg-white flex flex-col h-1/2">
+          <NuxtLink :to="`${article.articleUrl}`" class="relative h-full">
+            <img
+              :src="article?.articleThumbnailUrl"
+              alt=""
+              class="w-full h-full object-cover overflow-hidden rounded-t"
+            />
+            <span class="top-4 left-4 absolute rounded-md bg-primary py-1 px-2">{{
+              article?.source
+            }}</span>
+          </NuxtLink>
+        </div>
+
+        <div class="flex gap-4 flex-col p-4 h-1/2 bg-white rounded-b">
+          <div v-if="article.tags" class="flex flex-row text-secondary">
+            <span v-for="(tag, index) in article.tags" :key="index">
+              {{ tag }}
+              <span v-if="index < article.tags.length - 1" class="mx-2">|</span>
+            </span>
+          </div>
+          <div v-else class="text-white">
+            <span>|</span>
+          </div>
+          <div class="flex flex-col justify-between flex-wrap h-full" >
+            <h3 class="text-2xl font-extrabold text-ellipsis line-clamp-3 whitespace-break-spaces overflow-hidden" >
+              Je suis un titre Je suis un titre Je suis un titre Je suis un titre Je suis
+              un titre Je suis un titre Je suis un titre Je suis un titre Je suis un titre
+              Je suis un titre
+            </h3>
+            <div class="flex justify-between">
+              <p class="font-light">{{ formatDate(article.datePublication) }}</p>
+              <button type="">
+                <NuxtLink
+                  :to="`${article.articleUrl}`"
+                  class="rounded-md bg-primary/90 text-white hover:bg-primary py-1 px-2"
+                  _target="blank" >Lire sur {{ article.articleUrlOrigin }}
+                  </NuxtLink >
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-
-<script>
-  definePageMeta({
-    layout: "pr",
-  });
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      articles: null
-    }
-  },
-  methods: {
-    async fetchArticles() {
-      try {
-        const response = await axios.get('https://strapi.roulerpouraider.fr/api/articles', {
-          headers: {
-            'Authorization': `Bearer 197719aa439f651404f73e67c7f0bbc1fe3aaa23ec9d1f519aef360fd6670278ecf7ca1546548a9858cf5ed37e1da4986ba55c078b7050a2d76be451a1799830a790c3bc107f9a7ef8e0c3942fdad3e576dc4ba4a7fe0cd6a32de9400cfda9a3090099ed17259aeee52970a7f5c2a557e6ee6f848d4ce95ee53bbf0a18ec3fd0`
-          }
-        });
-        console.log(response.data.data);
-        this.articles = response.data.data;
-        console.log(this.articles[0].attributes.title);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
-  mounted() {
-    this.fetchArticles();
-  },
+<script setup lang="ts">
+interface Article {
+  id: number;
+  source: string;
+  tags: string[];
+  title: string;
+  articleTvThumbnailUrl: string;
+  articleUrl: string;
+  articleUrlOrigin: string;
+  datePublication: number;
 }
+
+const articles = ref<Article[]>([]);
+
+const runtimeConfig = useRuntimeConfig();
+const { public: { strapiBaseUrl, strapiToken } } = runtimeConfig;
+
+const formatDate = (date: string): string => {
+  const monthNames = [
+    "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+    "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+  ];
+
+  const dateObj = new Date(date);
+  const day = dateObj.getDate();
+  const monthIndex = dateObj.getMonth();
+  const year = dateObj.getFullYear();
+
+  return `${day} ${monthNames[monthIndex]} ${year}`;
+};
+
+const transformArticleObject = (articleTvData) => {
+  return articleTvData.map((data) => {
+    const { thumbnail, ...otherAttributes } = data.attributes;
+    return {
+      ...otherAttributes,
+      articleThumbnailUrl: strapiBaseUrl + thumbnail.data.attributes.url,
+      id: data.id,
+    };
+  });
+};
+
+const getArticle = async () => {
+  const url = `${strapiBaseUrl}/api/articles?populate=*`;
+
+  const { data, pending, error } = await useFetch(url, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${strapiToken}`,
+    },
+  });
+
+  if (!error.value && !pending.value && data.value) {
+    articles.value = transformArticleObject(data.value.data);
+  } else {
+    console.error(error);
+  }
+
+  console.log("reportagde", articles.value);
+};
+
+onMounted(() => {
+  getArticle();
+});
 </script>
--- pages/nos-retombees-presse/nos-reportages-tv.vue
+++ pages/nos-retombees-presse/nos-reportages-tv.vue
@@ -1,44 +1,117 @@
 <template>
   <div>
-    <h1>Nos reportages TV</h1>
-    <ul>
-      <li v-for="article in articles" >{{ article.attributes.title }}</li>
-    </ul>
+    <section class="flex flex-wrap bg-gray-200 pt-10">
+      <div
+        v-for="(reportage, index) in reportages"
+        :key="index"
+        class="px-4 mb-8 w-4/12 flex flex-col"
+      >
+        <div class="bg-white flex flex-col h-1/2">
+          <NuxtLink :to="`${reportage.reportageUrl}`" class="relative h-full">
+            <img
+              :src="reportage?.reportageTvThumbnailUrl"
+              alt=""
+              class="w-full h-full object-cover overflow-hidden rounded-t"
+            />
+            <span class="top-4 left-4 absolute rounded-md bg-primary py-1 px-2">{{
+              reportage?.source
+            }}</span>
+          </NuxtLink>
+        </div>
+
+        <div class="flex gap-4 flex-col p-4 h-1/2 bg-white rounded-b">
+          <div v-if="reportage.tags" class="flex flex-row text-secondary">
+            <span v-for="(tag, index) in reportage.tags" :key="index">
+              {{ tag }}
+              <span v-if="index < reportage.tags.length - 1" class="mx-2">|</span>
+            </span>
+          </div>
+          <div v-else class="text-white">
+            <span>|</span>
+          </div>
+          <div class="flex flex-col justify-between flex-wrap h-full" >
+            <h3 class="text-2xl font-extrabold text-ellipsis line-clamp-3 whitespace-break-spaces overflow-hidden" >
+              {{ reportage.title }}
+            </h3>
+            <div class="flex justify-between">
+              <p class="font-light">{{ formatDate(reportage.dataPublication) }}</p>
+              <button type="">
+                <NuxtLink
+                  :to="`${reportage.reportageUrl}`"
+                  class="rounded-md bg-primary/90 text-white hover:bg-primary py-1 px-2"
+                  _target="blank" >Regarder sur {{ reportage.reportageUrlOrigin }}
+                  </NuxtLink >
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-
-<script>
-import axios from 'axios';
-
-definePageMeta({
-  layout: "pr",
-});
-
-export default {
-  data() {
-    return {
-      articles: null
-    }
-  },
-  methods: {
-    async fetchArticles() {
-      try {
-        const response = await axios.get('https://strapi.roulerpouraider.fr/api/articles', {
-          headers: {
-            'Authorization': `Bearer 70e795be5b1a9c2f3a2cab6d11194c489d32ebd0b79c5d8ff944299e1966ebcaaa1661ae60480585178ca267057a02a8093d3cd256094950fd25ae57621cdf1de1ab393f2819d206d6eb049e68eb2fdaa99a83e071868f02a7dbd9af824c70e98af663272bf9169625747d337478632e4b48d02a478276fe2d8336fdc9d1b0a8`
-          }
-        });
-        console.log(response.data.data);
-        this.articles = response.data.data;
-        console.log(this.articles[0].attributes.title);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
-  mounted() {
-    this.fetchArticles();
-  },
+<script setup lang="ts">
+interface Reportage {
+  id: number;
+  source: string;
+  tags: string[];
+  title: string;
+  reportageTvThumbnailUrl: string;
+  reportageUrl: string;
+  reportageUrlOrigin: string;
+  datePublication: number;
 }
+
+const reportages = ref<Reportage[]>([]);
+const runtimeConfig = useRuntimeConfig();
+const {
+  public: { strapiBaseUrl, strapiToken },
+} = runtimeConfig;
+
+const formatDate = (date: string): string => {
+  const monthNames = [
+    "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+    "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+  ];
+
+  const dateObj = new Date(date);
+  const day = dateObj.getDate();
+  const monthIndex = dateObj.getMonth();
+  const year = dateObj.getFullYear();
+
+  return `${day} ${monthNames[monthIndex]} ${year}`;
+};
+
+const transformReportageTvObject = (reportageTvData) => {
+  return reportageTvData.map((data) => {
+    const { thumbnail, ...otherAttributes } = data.attributes;
+    return {
+      ...otherAttributes,
+      reportageTvThumbnailUrl: strapiBaseUrl + thumbnail.data.attributes.url,
+      id: data.id,
+    };
+  });
+};
+
+const getReportages = async () => {
+  const url = `${strapiBaseUrl}/api/reportage-tvs?populate=*`;
+
+  const { data, pending, error } = await useFetch(url, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${strapiToken}`,
+    },
+  });
+
+  if (!error.value && !pending.value && data.value) {
+    reportages.value = transformReportageTvObject(data.value.data);
+  } else {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  getReportages();
+});
 </script>
