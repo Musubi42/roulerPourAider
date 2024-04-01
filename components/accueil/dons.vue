d228bf55143726943cb59c0c83877ec986ee4a72
Musubi42 <raphaelthi59@gmail.com>
2024-04-01 21:03:51 +0200

    WIP

--- components/accueil/dons.vue
+++ components/accueil/dons.vue
@@ -212,36 +212,39 @@ const dayLeft = () => {
   return days;
 };
 
-const getDonations = async () => {
+const getDonations = () => {
   // Check if data is already stored in localStorage
-  const storedDonations = localStorage.getItem('donations');
-  if (storedDonations) {
-    donations.value = JSON.parse(storedDonations);
-    if (donations.value.length > 0) {
-      currentDonations.value = donations.value[0];
-    }
-    updateDonationGoals();
-    return; // End the function here as we have loaded the data
-  }
+  // const storedDonations = localStorage.getItem('donations');
+  // if (storedDonations) {
+  //   donations.value = JSON.parse(storedDonations);
+  //   if (donations.value.length > 0) {
+  //     currentDonations.value = donations.value[0];
+  //   }
+  //   updateDonationGoals();
+  //   return; // End the function here as we have loaded the data
+  // }
 
   // Fetch data if not stored
   const url = "https://donation-api.roulerpouraider.fr/donations";
-  const { data, pending, error } = await useFetch(url, {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-      },
+  const { data, pending, error } = useAsyncData('dons', () => {
+  return $fetch(url, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
+});
+
 
   if (!error.value && !pending.value && data.value) {
     donations.value = transformDonations(data.value);
     updateDonationGoals();
-    if (donations.value.length > 0) {
-      currentDonations.value = donations.value[0];
-    }
+    // if (donations.value.length > 0) {
+    //   currentDonations.value = donations.value[0];
+    // }
 
     // Store the data in localStorage
-    localStorage.setItem('donations', JSON.stringify(donations.value));
+    // localStorage.setItem('donations', JSON.stringify(donations.value));
   } else {
     console.error(error);
   }
@@ -271,6 +274,8 @@ const updateDonationGoals = () => {
     }
   }
 };
+
+getDonations();
 
 const formatNumber = (value) => {
   const parts = value.toString().split(/(?=(?:...)*$)/);
@@ -317,6 +322,6 @@ onMounted(() => {
 
 
 onBeforeMount(async () => {
-  await getDonations();
+  // await getDonations();
 });
 </script>
