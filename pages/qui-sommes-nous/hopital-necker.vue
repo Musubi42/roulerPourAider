0ad32edc30047a88c44d8afcb0cc009b1e9bacc8
Musubi42 <raphaelthi59@gmail.com>
2024-04-20 16:42:05 +0200

    WIP

--- pages/qui-sommes-nous/hopital-necker.vue
+++ pages/qui-sommes-nous/hopital-necker.vue
@@ -2,22 +2,22 @@
   <section class="relative py-16">
     <!-- En attendant d'avoir les éléments de déco, filigrane -->
 
-    <section class="flex flex-col md:flex-row w-full h-screen mb-8">
+    <section class="flex flex-col md:flex-row w-full h-full md:h-screen mb-0 md:mb-8">
       <!-- PP -->
       <div class="h-full w-full md:w-[47%] bg-[#F0EEEE] flex justify-center">
         
         <!-- <NuxtImg src="/oui/image00025.jpeg" alt="Photo de l'hôpital Necker" 
         class="h-full w-full object-cover rounded-lg" />-->
         <!-- <div style="mask-image: url('/mask.svg'); mask-size: cover;"> -->
-        <img src="/necker.jpeg" alt="" class="w-4/5 m-auto" />
+        <img src="/necker.webp" alt="" class="w-4/5 m-auto" />
         <!-- </div> -->
       </div>
       <!-- Texte -->
       <div
-        class="flex flex-col h-full w-full md:w-[53%] content-between gap-6 text-xl items-center py-16"
+        class="flex flex-col h-full w-full md:w-[53%] content-between gap-6 text-xl items-center py-6"
       >
-        <h1 class="font-bold text-4xl text-secondary">Hôpital Necker-Enfants malades</h1>
-        <div class="text-justify px-12 flex flex-col gap-12 text-xl h-auto my-auto" >
+        <h1 class="font-bold px-4 md:px-12 text-4xl text-secondary text-center">Hôpital Necker-Enfants malades</h1>
+        <div class="text-justify px-4 md:px-12 flex flex-col gap-12 text-xl h-auto my-auto" >
           <p>L’hôpital Necker-Enfants malades est <b>l’hôpital pédiatrique de référence en France.</b></p>
         <p>Son <b>expertise</b> dans la prise en charge des <b>pathologies de l’enfant</b> complétée par plusieurs <b>spécialités adultes</b> en fait un <b>établissement de renommée internationale.</b></p>
         <p><b>L’innovation</b> est au cœur de la stratégie de l’hôpital Necker. Pour mener à bien ses projets et développer l’ensemble de ses <b>missions de soin, de recherche et d’enseignement</b>, l’implication des donateurs est déterminante.</p>
@@ -27,20 +27,20 @@
 
     <section>
       <h2 class="font-bold text-3xl mb-4">Chiffres clés de l'hôpital</h2>
-      <div class="bg-[#F7F7FA] rounded-lg flex flex-col px-10 py-5 gap-6">
+      <div class="bg-[#F7F7FA] rounded-lg flex flex-col px-4 md:px-10 py-5 gap-6">
         <!-- Mettre 3 par div -->
         <div class="flex flex-row justify-between">
           <div
             ref="addToRefs"
             v-for="(item, index) in keyNumbers.slice(0, 3)"
             :key="index"
-            class="w-1/4"
+            class="w-1/3 md:w-1/4 flex items-center flex-col"
           >
             <CountUp
-              class="font-bold text-primary text-3xl"
+              class="font-bold text-primary text-xl md:text-3xl"
               :end="isVisible[index] ? item.number : 0"
             />
-            <p class="flex flex-wrap">{{ item.text }}</p>
+            <p class="flex flex-wrap text-center">{{ item.text }}</p>
           </div>
         </div>
         <!-- Seconde ligne -->
@@ -49,13 +49,13 @@
             ref="addToRefs"
             v-for="(item, index) in keyNumbers.slice(3)"
             :key="index"
-            class="w-1/4"
+            class="w-1/3 md:w-1/4 flex items-center flex-col"
           >
             <CountUp
-              class="font-bold text-primary text-3xl"
+              class="font-bold text-primary text-xl md:text-3xl"
               :end="isVisible[index] ? item.number : 0"
             />
-            <p class="flex flex-wrap">{{ item.text }}</p>
+            <p class="flex flex-wrap text-center">{{ item.text }}</p>
           </div>
         </div>
       </div>
