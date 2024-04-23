7f5b088c3fba12ddfe7a861127dbce2c3ab8b832
Musubi42 <raphaelthi59@gmail.com>
2024-04-23 15:34:41 +0200

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="mx-4">
-    <section class="mt-20" >
+    <p class="quote mt-10 mx-20 text-lg text-secondary text-justify">Nous sommes deux étudiants de l’Oise à mener un défi à la fois sportif et caritatif. Nous avons créé l’association Rouler pour aider en 2019 afin de récolter des dons pour l’Hôpital Necker – Enfants malades AP-HP à Paris. Cette récolte de dons, nous voulons la mener avec un défi sportif d’ampleur car nous sommes convaincus que le sport est un des meilleurs moyens de nous rassembler et véhicule des valeurs de solidarité et de dépassement qui collent parfaitement avec ce pour quoi nous agissons.</p>
+    <section class="mt-10" >
       <h2 class="font-semibold text-3xl text-secondary">Un défi sportif ambitieux</h2>
+      <!-- TODO : rajouter le text du qui sommes nous -->
       <AccueilChiffresCles />
     </section>
 
@@ -9,6 +11,7 @@
       <h2 class="font-semibold text-3xl text-secondary">Un tour de France pour les enfants hospitalisés</h2>
 
       <div class="flex flex-col md:flex-row w-full mt-6 md:mt-6" >
+      <!-- TODO : Add le filigranne que j'ai déjà -->
         <div class="w-full md:w-1/2 h-auto flex flex-col justify-around items-center" >
           <img src="/velo.png" alt="" class="w-max">
           <img class="object-contain" src="/1ere-edition/map.png" alt="">
@@ -45,6 +48,14 @@
   scroll-snap-type: y mandatory;
   overflow-y: auto; /* Ensure the container can scroll */
   height: 100%; 
+}
+
+.quote::before {
+  content: "“";
+  font-size: 3em;
+  top: -10px;
+  left: -20px;
+  color: #72BC7A;
 }
 
 </style>
